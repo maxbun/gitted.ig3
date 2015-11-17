9590ae771971b1a7cbb365fc4d5d881f42d464e9
Maxence Bunel <maxencebunel77@gmail.com>
2015-11-17 17:53:32 +0100

    TP end

--- actual/tree/ig/vecteur/parcs-naturels-regionaux.point.sh
+++ actual/tree/ig/vecteur/parcs-naturels-regionaux.point.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 ogr2ogr -f "ESRI Shapefile" shp/ parcs-naturels-regionaux.lonlat.vrt 
+ogr2ogr -f "ESRI Shapefile" shp_balades/ parcs-naturels-regionaux.balades.vrt 
+ogr2ogr -f "ESRI Shapefile" shp_balades_a_velo/ parcs-naturels-regionaux.balades-velo.vrt 
+ogr2ogr -f "ESRI Shapefile" shp_balades_a_velo2/ parcs-naturels-regionaux.balades-velo2.vrt 
+ogr2ogr -f "ESRI Shapefile" shp_lambert93_vrt/ parcs-naturels-regionaux.lambert93.vrt 
+
